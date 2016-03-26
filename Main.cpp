72d82eb885f9773cccc4a74c9327fe30d9332bf9
Aditya <aditya>
2016-03-26 17:00:00 +0530

    Modified Main.cpp : Added Usage info

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,17 @@ using namespace std;
 using namespace cv;
 int main(int argc, char** argv)
 {
-
+    if(argc < 4)
+    {
+        cout << argv[0] << endl;
+        cout << "Usage : <command> [<args>]" << endl;
+        cout << "List of commands :" << endl << endl;
+        cout << "train" << endl;
+        cout << "  syntax : " << argv[0] << "train <InputFile> <KnowledgeOutputFile>" << endl << endl;
+        cout << "predict" << endl;
+        cout << "  syntax : " << argv[0] << "predict <InputFile> <KnowledgeInputFile> <SuccessOutputFile> <ErrorOutputFile>" << endl << endl;
+        cout << "update" << endl;
+        cout << "  syntax : " << argv[0] << "update <InputFile> <KnowledgeFile>" << endl << endl;
+    }
     return 0;
 }
