5e9ae083886d833c92d1772fbf8012281f7e4130
Aditya <aditya>
2016-03-26 21:25:24 +0530

    Modified Train.cpp : Added code to Train(), Iterate over all students' images and create ImageVector and LabelVector

--- Train.cpp
+++ Train.cpp
@@ -11,4 +11,19 @@ using namespace cv;
 
 void Train(string InputFileName, string KnowledgeOutputFileName)
 {
+    vector<Mat> ImageVector;
+    vector<int> LabelVector;
+
+    // Read Class CSV File to get list of students and their CSV files
+    vector<vector<string>> ClassData = ReadCSVFile(InputFileName);
+    for( int ii = 0 ; ii < ClassData.size() ; ii++ )
+    {
+        // Read Student CSV file to get list of his/her images
+        vector<vector<string>> StudentData = ReadCSVFile(ClassData[ii][1]);
+        for( int jj = 0 ; jj < StudentData.size() ; jj++ )
+        {
+            ImageVector.push_back(imread(StudentData[jj][0].c_str(),0));
+            LabelVector.push_back(stoi(ClassData[ii][0]));
+        }
+    }
 }
