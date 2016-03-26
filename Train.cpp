4034ede071470f22debb51517d588719fe9de49a
Aditya <aditya>
2016-03-26 22:00:53 +0530

    Modified Train.cpp : Added a todo message

--- Train.cpp
+++ Train.cpp
@@ -26,4 +26,6 @@ void Train(string InputFileName, string KnowledgeOutputFileName)
             LabelVector.push_back(stoi(ClassData[ii][0]));
         }
     }
+    
+    /// Todo Create Recogintion model and train using training data.
 }
