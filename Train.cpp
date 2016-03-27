302d977099d5fe090bd576d7c0f6ff08e604c81d
Aditya <aditya>
2016-03-27 09:50:07 +0530

    Modified Train.cpp : Updated Train()

--- Train.cpp
+++ Train.cpp
@@ -8,6 +8,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
+
 using namespace std;
 using namespace cv;
 using namespace cv::face;
@@ -17,19 +18,14 @@ void Train(string InputFileName, string KnowledgeOutputFileName)
     vector<Mat> ImageVector;
     vector<int> LabelVector;
 
-    // Read Class CSV File to get list of students and their CSV files
-    vector<vector<string>> ClassData = ReadCSVFile(InputFileName);
-    for( int ii = 0 ; ii < ClassData.size() ; ii++ )
+    // Read Student CSV file to get list of his/her images
+    vector<vector<string>> StudentData = ReadCSVFile(InputFileName);
+    for( int jj = 0 ; jj < StudentData.size() ; jj++ )
     {
-        // Read Student CSV file to get list of his/her images
-        vector<vector<string>> StudentData = ReadCSVFile(ClassData[ii][1]);
-        for( int jj = 0 ; jj < StudentData.size() ; jj++ )
-        {
-            ImageVector.push_back(imread(StudentData[jj][0].c_str(),0));
-            LabelVector.push_back(stoi(ClassData[ii][0]));
-        }
+        //Create Training Vectors
+        ImageVector.push_back(imread(StudentData[jj][1].c_str(),0));
+        LabelVector.push_back(stoi(StudentData[jji][0]));
     }
- 
 
     /// Todo Create Recogintion model and train using training data.
     /// Save Model to KnowledgeOutputFile
